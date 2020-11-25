d574a28931f2fc8554a179a6253702b2b9b49fb7
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:01:45 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan6/kambingstaticdankonstanta/PBO10K10119901Latihan6KambingStaticDanKonstanta.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan6.kambingstaticdankonstanta;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Kambing Static dan Konstanta
+ *
+ */
+
+class Mamalia
+{
+    public static int jumlahKambing;
+}
+
+public class PBO10K10119901Latihan6KambingStaticDanKonstanta {
+
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+}
